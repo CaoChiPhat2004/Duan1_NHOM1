c734016dece4d689b7dc27d426543ad11a098828
Vubakiem23 <151489301+Vubakiem23@users.noreply.github.com>
2025-03-03 22:05:29 +0700

    Add files via upload

---
+++ views/client/authen/form-register.php
@@ -0,0 +1,34 @@
+<?php
+if (isset($_SESSION['success'])) {
+    $class = $_SESSION['success'] ? 'alert-success' : 'alert-danger';
+
+    echo "<div class='alert $class alert-dismissible fade show' role='alert'> {$_SESSION['msg']} 
+            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+          </div>";
+
+    unset($_SESSION['success']);
+    unset($_SESSION['msg']);
+}
+?>
+
+<div class="container d-flex justify-content-center align-items-center min-vh-100">
+    <div class="card shadow-lg p-4" style="max-width: 400px; width: 100%;">
+        <h3 class="card-title text-center mb-4">register</h3>
+        <form action="<?= BASE_URL_ADMIN . '&action=register' ?>" method="post">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password:</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <div class="d-grid gap-2 mb-3">
+                <button type="submit" class="btn btn-primary">register</button>
+            </div>
+            <div class="text-center">
+                <a href="<?= BASE_URL_ADMIN . '&action=forgot-password' ?>" class="text-muted">Forgot your password?</a>
+            </div>
+        </form>
+    </div>
+</div>
