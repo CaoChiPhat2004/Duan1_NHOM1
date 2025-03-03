67741e9b6edfd195e36ff5f12531bf527e9bde44
Vubakiem23 <151489301+Vubakiem23@users.noreply.github.com>
2025-03-03 22:07:10 +0700

    Update Category.php

--- models/Category.php
+++ models/Category.php
@@ -3,4 +3,34 @@
 class Category extends BaseModel
 {
     protected $table = 'categories';
+
+    // Lấy tất cả danh mục, có thể sắp xếp theo ID giảm dần
+    public function getAll()
+    {
+        return $this->select('*', '1 = 1 ORDER BY id DESC');
+    }
+
+    // Tìm danh mục theo ID
+    public function getById($id)
+    {
+        return $this->find('*', 'id = :id', ['id' => $id]);
+    }
+
+    // Thêm danh mục mới
+    public function add($data)
+    {
+        return $this->insert($data);
+    }
+
+    // Cập nhật danh mục theo ID
+    public function updateById($id, $data)
+    {
+        return $this->update($data, 'id = :id', ['id' => $id]);
+    }
+
+    // Xóa danh mục theo ID
+    public function deleteById($id)
+    {
+        return $this->delete('id = :id', ['id' => $id]);
+    }
 }
